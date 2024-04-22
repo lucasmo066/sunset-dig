ba04b62e6408efdf185950aacb3997cce30af913
Lucas Moraes <lucasmo066@gmail.com>
2024-04-22 19:07:53 -0400

    spline scene

---
+++ src/components/Hero.tsx
@@ -0,0 +1,10 @@
+'use client'
+import Spline from '@splinetool/react-spline';
+
+export default function Hero() {
+  return (
+    <section className="flec flex-col object-cover">
+      <Spline scene="https://prod.spline.design/OIHZKF-QfaUqZJvC/scene.splinecode" />
+    </section>
+  );
+}
---
+++ src/components/Nav.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import Image from "next/image";
+import { ModeToggle } from "./ui/toggle-theme";
+
+
+export default function Nav() {
+  return (
+    <header>
+        <nav>
+            <ul className="flex items-center justify-center gap-4 p-4">
+                <Image 
+                    src="/vercel.svg"
+                    alt="logo"
+                    width={100}
+                    height={50}
+                    className="dark:invert"
+                    priority
+                />
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/about">About</a>
+                </li>
+                <li>
+                    <a href="/contact">Contact</a>
+                </li>
+                <li>
+                   <ModeToggle />
+                </li>
+
+            </ul>
+        </nav>
+    </header>
+  );
+}
