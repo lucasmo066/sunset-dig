e090ee9bf323901d288c771ed09f56dd0768492c
Lucas Moraes <lucasmo066@gmail.com>
2024-04-22 20:16:18 -0400

    hero is done with spline scene

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,22 @@
 'use client'
-// hero.tsx
+
 import Spline from '@splinetool/react-spline';
+import { Button } from './ui/button';
+import Link from 'next/link';
 
 export default function Hero() {
   return (
-    <section className="flex flex-col w-full h-screen overflow-hidden">
-      <div className="w-full h-screen/2">
-      <Spline scene="https://prod.spline.design/OIHZKF-QfaUqZJvC/scene.splinecode" />
+    <section className="relative flex flex-col w-full overflow-hidden">
+      <div className="w-full h-screen/2 relative z-10">
+        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-6">
+            <Button variant="outline" className="border border-primary text-primary bg-transparent mx-2">
+                <Link href="#pricing">See Plans</Link>
+            </Button>
+            <Button className="mx-2">
+                <Link href="/contact">Schedule Call</Link>
+            </Button>
+        </div>
+        <Spline scene="https://prod.spline.design/OIHZKF-QfaUqZJvC/scene.splinecode" />
       </div>
     </section>
   );
