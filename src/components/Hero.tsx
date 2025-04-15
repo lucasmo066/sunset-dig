cfeab52209555de323dbbc1e7f2141fc41f6f7f7
Lucas Moraes <lucasmo066@gmail.com>
2025-04-15 12:56:42 -0600

    changes to the hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 
 export function Hero() {
   return (
-    <WavyBackground className="h-1/2 mx-auto"> {/* Adjust the height here */}
+    <WavyBackground className="h-1/2 mx-auto px-10"> {/* Adjust the height here */}
       <div className="h-1/2 w-full flex flex-col items-center justify-center">
         {/* Wrap the h1 with motion.div */}
         <motion.div
@@ -16,8 +16,8 @@ export function Hero() {
           animate={{ y: 0 }} // Animate to y position 0 (normal position)
           transition={{ duration: 0.5, delay: 0.3 }} // Animation duration and delay
         >
-          <h1 className="text-7xl md:text-9xl text-center bg-gradient-to-br from-orange-300 to-orange-800 bg-clip-text font-semibold tracking-tight text-transparent pb-3 mb-4">
-            Sunset Digital
+          <h1 className="text-5xl md:text-6xl text-center bg-gradient-to-br from-orange-300 to-orange-800 bg-clip-text font-semibold tracking-tight text-transparent pb-3 mb-4">
+          Custom Websites That Fuel Your Business Growth.
           </h1>
         </motion.div>
         {/* The rest of the content */}
@@ -26,8 +26,8 @@ export function Hero() {
           animate={{ y: 0 }} 
           transition={{ duration: 0.5, delay: 0.3 }}
         >
-        <p className="text-2xl md:text-3xl mt-4 text-white font-extralight inter-var text-center">
-          Empowering Businesses with Cutting-Edge Digital Solutions
+        <p className="text-xl md:text-xl mt-4 text-white font-extralight inter-var text-center">
+        Skip the complexity and inflated costs of traditional agencies. We build, host, and maintain your website for one transparent fee—so you can focus on what matters most.
         </p>
         </motion.div>
         <motion.div
@@ -35,12 +35,13 @@ export function Hero() {
           animate={{ opacity: 1 }}
           transition={{ duration: 0.7, delay: 0.7 }}
         >
-        <div className="flex flex-row gap-8 items-center justify-center mt-20">
+        <div className="flex flex-row gap-8 items-center justify-center mt-10">
           <Link href="#pricing">
-          <Button variant="outline" className="mt-8 bg-transparent hover:bg-primary hover:border-primary border-2 border-white text-white">See Plans</Button>
+          <Button variant="outline" className="mt-8 bg-transparent hover:bg-primary hover:border-primary border-2 border-white text-white">See Plan & Pricing</Button>
           </Link>
-          <Link href="https://calendly.com/lmoraes-zay4/30min">
-          <Button className="mt-8">Schedule Call</Button>
+          {/* TODO: Add link to calendly */}
+          <Link href="">
+          <Button className="mt-8">Let’s Talk Growth</Button>
           </Link>
         </div>
         </motion.div>
